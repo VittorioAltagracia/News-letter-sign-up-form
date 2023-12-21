155c13164b9dbbff90bd0862567d56ee11c3e540
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-12-20 22:56:51 -0600

    challenge completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("test");
+const modal = new bootstrap.Modal(document.getElementById("confirmationModal"));
+const mainContainer = document.querySelector(".main-container");
+const emailInput = document.getElementById("email");
+const subscribeBtn = document.getElementById("subs");
+const customerEmail = document.querySelector(".customer-email");
+const errorLabel = document.getElementById("errorLabel");
+const dismissButton = document.querySelector(".dismiss-button");
+
+document.addEventListener("DOMContentLoaded", () => {
+  subscribeBtn.addEventListener("click", () => {
+    if (!validateEmail(emailInput.value)) {
+      emailInput.classList.add("text-danger");
+      errorLabel.classList.remove("hide");
+      return null;
+    }
+    displayModal();
+    parseSubsEmail(emailInput.value);
+  });
+
+  modal._element.addEventListener("hidden.bs.modal", () => {
+    emailInput.value = "";
+    emailInput.classList.remove("text-danger");
+    mainContainer.classList.remove("hide");
+  });
+});
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function displayModal() {
+  modal.show();
+
+  mainContainer.classList.add("hide");
+}
+
+function parseSubsEmail(email) {
+  const appendedDot = `${email}.`;
+  customerEmail.textContent = appendedDot;
+}
